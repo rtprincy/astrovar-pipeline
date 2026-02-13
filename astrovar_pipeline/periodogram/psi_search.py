cde4975bf689bd1b34360c13044c14d892d0f55e
Jesse Crans <crans.coding@gmail.com>
2026-02-13 19:59:23 +0100

    added verbose option to `freq_grid` for more clear debugging

--- astrovar_pipeline/periodogram/psi_search.py
+++ astrovar_pipeline/periodogram/psi_search.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 import numpy as np
 from typing import Dict, Any, Tuple
@@ -6,36 +5,42 @@ import importlib
 from astropy.timeseries import LombScargle
 from lk_stat_package import lk_stat
 
-def freq_grid(times,oversample_factor=10,f0=None,fn=None):
-    times=np.sort(times)
+
+def freq_grid(times, oversample_factor=10, f0=None, fn=None, verbose=False):
+    times = np.sort(times)
     df = 1.0 / (times.max() - times.min())
-    if (f0 is None) | (int(f0)==0):
+    if (f0 is None) | (int(f0) == 0):
         f0 = df
     if fn is None:
-        fn = 0.5 / np.median(np.diff(times)) 
-    print("Time: ",times)
-    print("Minimum freq: ", f0)
-    print("Maximum freq: ", fn)
-    print("Freq resolution prior oversampling: ", df)
-    print("Oversampling factor: ", oversample_factor)
+        fn = 0.5 / np.median(np.diff(times))
+
+    if verbose:
+        print("Time: ", times)
+        print("Minimum freq: ", f0)
+        print("Maximum freq: ", fn)
+        print("Freq resolution prior oversampling: ", df)
+        print("Oversampling factor: ", oversample_factor)
 
     return np.arange(f0, fn, df / oversample_factor)
 
-def psi_periodogram(time, mag, err, min_freq, max_freq, oversample=5) -> Dict[str, np.ndarray]:
+
+def psi_periodogram(
+    time, mag, err, min_freq, max_freq, oversample=5
+) -> Dict[str, np.ndarray]:
     """Compute Ψ-statistic = 2*LSP / theta, combining LS and LK.
     Returns dict with 'freq', 'lsp', 'theta', 'psi'.
     """
 
-    frequencies = freq_grid(time, oversample_factor=oversample, f0=min_freq, fn=max_freq)
+    frequencies = freq_grid(
+        time, oversample_factor=oversample, f0=min_freq, fn=max_freq
+    )
     periods = 1 / frequencies
 
     lsp = LombScargle(time, mag, err, nterms=1).power(
-                frequencies, method="cython", normalization="psd"
-            )
+        frequencies, method="cython", normalization="psd"
+    )
     theta = lk_stat(periods, mag, err, time)
 
-    psi = (2*lsp)/theta
+    psi = (2 * lsp) / theta
 
-
-
-    return {"freq":frequencies, "lsp":lsp, "theta":theta, "psi":psi}
+    return {"freq": frequencies, "lsp": lsp, "theta": theta, "psi": psi}
